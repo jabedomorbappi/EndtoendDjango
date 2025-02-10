92578a1d27c4cd08e2d4d1a9977417c85974b806
Jabed <jabedomorbappi100@gmail.com>
2025-02-11 00:28:58 +0600

    create a model contact

--- MainProject/MainProject/urls.py
+++ MainProject/MainProject/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,about,information
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',home)
+    path('',home,name='home_page'),
+    path('about/',about,name='about_page'),
+    path('info/',information,name='info_page')
+    
 ]
--- MainProject/MainProject/views.py
+++ MainProject/MainProject/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render,HttpResponse
 
-
+from tution.models import Contact
 def home(request):
-    return render(request,'home.html') 
+    
+    if request.method=="POST":
+        name=['Rahul','Rohit','Raj']
+    else:
+        name=["this is get method",'Rahul','Rohit','Raj']    
+    context={
+        "name":name
+    }
+    return render(request,'home.html',context) 
+
+def about(request):
+    number=[x for x in range(1,100)]
+    context={
+        "number":number}
+    return render(request,'about.html',context)
+def information(request):
+    
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        
+        obj=Contact(name=name,email=email,phone=phone)
+        obj.save()
+        
+    return render(request,'info.html')
